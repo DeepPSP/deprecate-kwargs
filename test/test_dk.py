45f5f5951c9885f1e2dcce24b04874e00d234156
WEN Hao <wenh06@gmail.com>
2022-05-29 20:58:05 +0800

    update examples

--- test/test_dk.py
+++ test/test_dk.py
@@ -13,8 +13,10 @@ except ModuleNotFoundError:
     from deprecate_kwargs import deprecate_kwargs
 
 
-@deprecate_kwargs([["new_arg_1", "old_arg_1"], ["new_arg_2", "old_arg_2"]])
-def some_func(old_arg_1: int, old_arg_2: int):
+@deprecate_kwargs(
+    [["new_arg_1", "old_arg_1"], ["new_arg_2", "old_arg_2"], ["new_kw", "old_kw"]]
+)
+def some_func(old_arg_1: int, old_arg_2: int, old_kw: int = 3):
     """
     Parameters
     ----------
@@ -22,22 +24,32 @@ def some_func(old_arg_1: int, old_arg_2: int):
         argument 1
     old_arg_2: int,
         argument 2
+    old_kw: int, default 3,
+        keyword argument
 
     """
-    return old_arg_1 + old_arg_2
+    return (old_arg_1 + old_arg_2) * old_kw
 
 
 def test_dk():
     """ """
 
     if sys.version_info[:2] <= (3, 6):
-        assert str(some_func.__signature__) == "(new_arg_1:int, new_arg_2:int)"
+        assert (
+            str(some_func.__signature__)
+            == "(new_arg_1:int, new_arg_2:int, new_kw:int=3)"
+        )
     else:
-        assert str(some_func.__signature__) == "(new_arg_1: int, new_arg_2: int)"
-    assert some_func(10, 20) == 30
-    assert (
-        some_func.__doc__
-        == "\n    Parameters\n    ----------\n    new_arg_1: int,\n        argument 1\n    new_arg_2: int,\n        argument 2\n\n    "
+        assert (
+            str(some_func.__signature__)
+            == "(new_arg_1: int, new_arg_2: int, new_kw: int = 3)"
+        )
+    assert some_func(10, 20, 4) == 120
+    assert some_func.__doc__ == (
+        "\n    Parameters\n    ----------"
+        "\n    new_arg_1: int,\n        argument 1"
+        "\n    new_arg_2: int,\n        argument 2"
+        "\n    new_kw: int, default 3,\n        keyword argument\n\n    "
     )
 
 
